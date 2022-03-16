222f1bbe9f667ffd6d552538f01164fce8d3ac68
Alexey Sutko <alexeysutko@gmail.com>
2022-03-16 15:48:35 +0300

    Removed accidently added view

--- web_site/services/photo/search.py
+++
@@ -1,8 +0,0 @@
-from models_module.models import Photo
-from django.shortcuts import get_list_or_404
-from service_objects.services import Service
-#from service_objects.fields import
-
-
-class PhotoSearch(Service):
-    pass
